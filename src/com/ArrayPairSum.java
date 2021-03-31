97591a48c746fc5887522b7e5a2a5ec6b94cf71e
Markus Brødsjø <markus.brodsjo@gmail.com>
2021-03-31 12:55:51 +0200

    Oppdaterte oppgave 2

--- src/com/ArrayPairSum.java
+++ src/com/ArrayPairSum.java
@@ -8,7 +8,7 @@ public class ArrayPairSum {
 
     public static void main(String[] args) {
 
-        int []a = {4, 2, 7, 6, -3, -1, -2, 42, 0, -42, 9, -4, 5, -5, -6, -7, -8, -99, 42, 11, 20, 1, 2, 3};
+        int[] a = {4, 2, 7, 6, -3, -1, -2, 42, 0, -42, 9, -4, 5, -5, -6, -7, -8, -99, 42, 11, 20, 1, 2, 3};
         printSumPairs(a,0);
 
     }
--- src/com/HighestAndLowest.java
+++ src/com/HighestAndLowest.java
@@ -1,27 +1,42 @@
 package com;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HighestAndLowest {
 
     public static void main(String[] args){
-        int[] number = new int[] {4, 2, 7, 6, -3, -1, -2, 42, 0, -42, 9, -4, 5, -5, -6, -7, -8, -99, 42, 11, 20, 1, 2, 3};
-        int highest = 0;
-        int lowest = 0;
-
-        for (int j : number) {
-            if (j > highest) {
-                highest = j;
-            } else if (j < lowest) {
-                lowest = j;
-            }
-
-        }
-        System.out.println("highest number: " + highest);
-        System.out.println("lowest number: " + lowest);
-        System.out.println(number[lowest]);
-        System.out.println(number[highest]);
-        
+        int[] arr = {4, 2, 7, 6, -3, -1, -2, 42, 0, -42, 9, -4, 5, -5, -6, -7, -8, -99, 42, 11, 20, 1, 2, 3};
+        highestAndLowest(arr);
     }
-    public static void highest(int[]arr){
+    public static void highestAndLowest(int[] arr){
+        int largestIndex = 0;
+        int smallestIndex = 0;
+        int largestNumber = 0;
+        int smallestNumber = 0;
+
+        // For loop over lista
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > largestNumber) {
+                largestIndex = i + 1;
+                largestNumber = arr[i];
+            } else if (arr[i] < smallestNumber) {
+                smallestIndex = i + 1;
+                smallestNumber = arr[i];
+            }
+        }
+        //Printer den høyeste nummeret + hva slags plasseing den er på
+        System.out.println("Largest index: ");
+        System.out.println(largestIndex);
+        System.out.println("Largest Number: ");
+        System.out.println(largestNumber);
+
+        //Printer den laveste nummeret + hva slags plasseing den er på
+        System.out.println("Smallest Number: ");
+        System.out.println(smallestNumber);
+        System.out.println("Smallest index: ");
+        System.out.println(smallestIndex);
+
 
     }
 }
